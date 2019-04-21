78ebeda431d548ef4941ef76cfbebf3cc668abe3
Tyler Simmons <tylersimmons@Tylers-MBP.fios-router.home>
2019-04-20 21:37:28 -0400

    updated driver

--- pi-lit-driver.py
+++ pi-lit-driver.py
@@ -7,7 +7,17 @@ import os
 import math
 
 # rpi library import
-# from rpi_ws281x import *
+from rpi_ws281x import *
+
+# strip variables
+LED_COUNT = 30
+LED_PIN = 18
+LED_FREQ_HZ = 800000
+LED_DMA = 10
+LED_BRIGHTNESS = 255
+LED_INVERT = False
+LED_CHANNEL = 0
+
 
 #states
 OFF = '0#0#0'
@@ -16,6 +26,29 @@ WHITE = '255#255#255'
 # globals
 LED_STATE_CACHE = []
 COMMAND_BUFFER = {}
+
+#Listen for JSON input
+def listenForJson():
+	commandObjString = input()
+	commandObjParsed = json.loads(commandObjString)
+	configObj = commandObjParsed['config']
+	commandArray = commandObjParsed['config']['commandArray']
+	print(commandArray)
+	for i in range(len(commandArray)):
+		if commandArray[i]['effect'] == 'custom':
+			populateBufferFromCustomCommand(commandArray[i])
+		elif commandArray[i]['effect'] == 'solid':
+			colorSolid(commandArray[i]['range'], commandArray[i]['range']['color']['r'], commandArray[i]['range']['color']['g'], commandArray[i]['range']['color']['b'])
+		else:
+			return 1
+
+#color solid
+def colorSolid(range, r, g, b):
+	for i in range(150):
+		for pixel in range:
+			LED_STATE_CACHE[i][pixel] = str(r) + '#' + str(g) + '#' + str(b)
+		
+
 
 #initialize the led strip refresh cache with all white LED's
 def initStateCache():
@@ -51,7 +84,7 @@ def loadCacheFromBuffer():
 			LED_STATE_CACHE[refresh][i] = currentColor
 		
 
-def runStrip():
+def runStrip(strip):
 	while True:
 		print('Start loop')
 		for refreshCounter in range(150):
@@ -61,8 +94,8 @@ def runStrip():
 				ledrep += LED_STATE_CACHE[refreshCounter][pixel]
 				rgbVals = LED_STATE_CACHE[refreshCounter][pixel].split('#')
 				#using ws281x
-				#strip.setPixelColorRGB(pixel, rgbVals[0], rgbVals[1], rgbVals[2])
-				#strip.show()
+				strip.setPixelColorRGB(pixel, rgbVals[0], rgbVals[1], rgbVals[2])
+				strip.show()
 			print(ledrep)
 		time.sleep(0.062)
 
@@ -71,8 +104,18 @@ if __name__ == '__main__':
 	sampleFileLink = open('sample.JSON', 'r')
 	sampleFile = sampleFileLink.read()
 	parsedJson = json.loads(sampleFile)
+	
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+	strip.begin()
+	
 	initStateCache()
 	populateBufferFromCustomCommand(parsedJson[2])
 	loadCacheFromBuffer()
-	runStrip()
+
+	#t1 = threading.Thread(target=listenForJson)
+	t2 = threading.Thread(target=runStrip, args=(strip, ))
+	testThread = threading.Thread(target=colorSolid, args=([0,1,2,3,4,5,6,7,8,9,10], 255, 0, 0))
+
+	t2.start()
+	testThread.start()
 
